3f42175a950ef222d7256dd1112dd033d6166a86
rftbyr <52688364+rftbyr@users.noreply.github.com>
2024-12-21 21:30:10 +0300

    Add files via upload

---
+++ Promodoro/Promodoro Watch App/PromodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromodoroApp.swift
+//  Promodoro Watch App
+//
+//  Created by Rıfat on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Promodoro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
